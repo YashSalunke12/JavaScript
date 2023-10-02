f00490a8dcce60e726afd7e6a3177a7da5e8bbd1
Yash <salunkeyaash12@gmail.com>
2023-10-02 12:09:33 +0530

    Variables in JavaScript

---
+++ Lecture-2.js
@@ -0,0 +1,4 @@
+let a = 3; // a contains 3
+console.log(a);
+a = "Yash";
+console.log(a);
---
+++ Lecture-3.js
@@ -0,0 +1,11 @@
+var a = 45;
+var a = "p";
+let b = "Yash";
+var c = null;
+var d = undefined;
+
+{
+    let b = 'this';
+    console.log(b);
+}
+console.log(b);
