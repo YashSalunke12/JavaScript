e30efec60d2f4a80b594bbeae2968c33d6020b83
Yash <salunkeyaash12@gmail.com>
2023-10-04 06:52:11 +0530

    Problem Set 1

---
+++ Practice-Set-1.js
@@ -0,0 +1,30 @@
+//Question-1
+let a = "Yash";
+let b = 49;
+console.log(a+b);
+
+//Question-2
+console.log(typeof (a+b));
+
+//Question-3
+const c = {
+    Yash: 39,
+    Sarthak: true,
+    Patil: false
+}
+// b = 39;
+console.log(c);
+
+//Question-4
+c["Nikhil"] = 50;
+console.log(c);
+
+//Question-5
+const dictionary = {
+    consider: "deem to be",
+    accord: "concurrence of opinion",
+    practice: "a customary way of operation or behavior"
+}
+console.log(dictionary);
+console.log(dictionary.accord);
+console.log(dictionary['consider']);
